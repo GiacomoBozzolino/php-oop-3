38ec96ff30d7adbe4b76e0bf76ab128792774c9d
GiacomoBozzolino <g.bozzolino@gmail.com>
2023-07-11 16:48:48 +0200

    [FEAT] lavoro funzione stampa email e classe sms

--- index.php
+++ index.php
@@ -17,10 +17,12 @@
     }
 
     // CHILD CLASS
+    // CLASSE MAIL
     class Email extends Comunication{
         private $notification;
+        public $print;
 
-        public function __construct($_sender, $_recipient, $_message, $notification){
+        public function __construct(string $_sender, string $_recipient, string $_message, bool $notification){
             parent::__construct($_sender, $_recipient, $_message);
             $this->notification = $notification;
 
@@ -29,30 +31,70 @@
                 return "email inviata";
             }
 
-        
-            public function setResponse ($notification) {
-                if ($this->notification === true ) {
-                    echo $this->sendMessage();
-                }
-                else {
-                   echo "email non inviata";
-                }
+        // funzione  rispsota email
+        public function setResponse () {
+            if ($this->notification === true ) {
+                echo $this->sendMessage();
             }
-    
-          
+            else {
+                echo "email non inviata";
+            }
+        }
+        // funzione stampa
+        public function setPrint () {
+            if ($this->print === true ) {
+                echo 'email stampata';
+            }
+            
+        }
 
+        public function getPrint (){
+            return $this->print;
+        }
+
+
+    
     }
 
+
+    // CLASSE MESSAGE
+    class Message extends Comunication{
+        private $notification;
+
+        public function __construct(string $_sender, string $_recipient, string $_message, bool $notification){
+            parent::__construct($_sender, $_recipient, $_message);
+            $this->notification = $notification;
+
+        }
+
+        public function sendMessage(){
+                return "sms letto";
+            }
+
+        public function setResponse () {
+            if ($this->notification === true ) {
+                echo $this->sendMessage();
+            }
+            else {
+                 echo "sms non letto";
+            }
+        }
+    }
+
+
     // ISTANZE DI PROVA
-    $prova = new Comunication ('Io','tu','ciao');
+    $prova = new Comunication ('Io','tu','addio');
     var_dump($prova);
+   
 
     $email_1 = new Email ('Io','tu','ciao amico',true);
-    $email_1->setResponse(true);
+    $email_1->setResponse();
+    $email_1->print = false;
+    $email_1->setPrint();
     
 
     $email_2 = new Email ('Io','tu','ciao amico',false);
-    $email_2->setResponse(true);
+    $email_2->setResponse();
     
 
 
